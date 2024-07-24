dd95e5752e07488b3d8426d4be038cf4183ccf2e
gvrsrg <gvrsrg@gmail.com>
2024-07-24 14:41:05 +0300

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(express.urlencoded({extended: true})) // to parse urlencoded data
+app.use(express.json()) // to parse json data
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(cors())
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server on port ${PORT}`);
+})
+
+
