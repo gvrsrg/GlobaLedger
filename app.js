6792ba0ba755f84fdee0f2406ac0bee51ae55200
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-07-28 12:49:28 +0300

    add income/expense, remove recurring, budget percentage data

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const { transactionRouter } = require('./routes/transactionRouter.js');
 const { _createUser, _getUserByEmail } = require('./models/userModel.js');
 const { _getAllUserCategories } = require('./models/categoryModel.js');
 const { _getAllUserAccounts } = require('./models/accountModel.js');
-const { _getUserTransactionsForPeriod } = require('./models/transactionModel.js');
+const { _getUserTransactionsForPeriod, _getUserBudgetForPeriod } = require('./models/transactionModel.js');
+const { getUserBudgetForPeriod } = require('./controllers/transactionController.js');
 
 const app = express()
 const secret = process.env.SESSION_SECRET;
@@ -36,14 +37,20 @@ app.get('/', async (req, res) => {
     let user = req.session.user;
     if(user){
         const userAccounts = await  _getAllUserAccounts({userid: user.userid, email: user.email});
-        const userCategories = await _getAllUserCategories({userid: user.userid, email: user.email});
         const userTransactions = await _getUserTransactionsForPeriod({
             fromdate: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString(),
             todate: new Date().toISOString(),
             userid: user.userid,
             email: user.email
         })
-        res.render('index', {loggedIn: !!user, user, accounts: userAccounts, categories: userCategories, incomes: [], transactions: userTransactions, currencies});
+        const userCategories = await _getAllUserCategories({userid: user.userid, email: user.email});
+        const userBudgets = await _getUserBudgetForPeriod({
+            fromdate: new Date(new Date().getFullYear(), new Date().getMonth(), 1).toISOString(),
+            todate: new Date().toISOString(),
+            userid: user.userid,
+            email: user.email
+        })
+        res.render('index', {loggedIn: !!user, user, accounts: userAccounts, budgets: userBudgets, categories: userCategories, incomes: [], transactions: userTransactions, currencies});
     } else {
         res.render('index', {loggedIn: !!user, user})
     }
