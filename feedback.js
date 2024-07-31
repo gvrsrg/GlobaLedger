0821cf29e343c5e3111bdc9eb11678fd0b6627ab
DiGureev <dianagureev@gmail.com>
2024-07-31 22:19:14 +0300

    feedback

---
+++ feedback.js
@@ -0,0 +1,9 @@
+// Amazing project! So stylish and slick. I love that you use Neon DB, which allows me to just connect to your project without having to set anything up. And also it makes your project much stable.
+// Your project has a good structure and you serve all the pages from the server - that is impressive. 
+// At first sight I was a bit confused with 0 account and category. So maybe it would be worth telling the user somewhere that you have settings where the user needs to create an account and all that. Or give a default account and categories that can be changed later.
+
+// But very good job guys! Deploy it so people can really use it :) 
+
+// I don't have much to say about your code, great job!
+
+// Just a note that even though you added your .env to .gitignore, it still shows up. I assume you did this for me so that I could connect, but if not, just know that if you once pushed .env (or any other file) to git and then added it to .gitignore, it will not be ignored. You will have to manually remove it from the repo.
